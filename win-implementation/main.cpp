1ef4c24ded9969d255d64e777ea2204e5ca45c73
Anton Dmitrievsky <admitrievsky@gmail.com>
2020-09-25 00:33:31 +0500

    do not init note, do it in instrument handling

--- win-implementation/main.cpp
+++ win-implementation/main.cpp
@@ -144,7 +144,6 @@ public:
                 return;
             if (note < 0x80) {
                 note -= 2;
-                channel.init_tone(note);
                 channel_state.current_note = note;
                 channel_state.instrument_playback_ptr = channel_state.instrument_ptr + 1;
                 return;
